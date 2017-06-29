a2ae8b4523138a718eddaeddd142580b86728c63
matrix2reality <matrix2reality@gmail.com>
2017-06-29 10:57:31 +0100

    SpellChecker

---
+++ spellcheker/SpellCheck_Test.py
@@ -0,0 +1,19 @@
+
+import unittest
+from SpellClass import SpellChecker
+class TestSpellChecker(unittest.TestCase):
+
+    def setUp(self):
+        self.spellChecker = SpellChecker()
+        self.spellChecker.load_words('spell.words')
+
+    def test_spell_checker(self):
+		self.assertTrue(self.spellChecker.check_word('zygotic'))
+		self.assertFalse(self.spellChecker.check_words('zygotic mistasdas elementary'))
+		self.assertTrue(self.spellChecker.check_words('our first correct sentence'))
+		self.assertTrue(self.spellChecker.check_words('our first correct sentence.'))
+		self.assertFalse(self.spellChecker.check_words('zygotic mistasdas spelllleeeing elementary'))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ spellcheker/SpellClass.py
@@ -0,0 +1,29 @@
+
+
+class SpellChecker:
+	def __init__(self):
+		self.words = []
+
+	def load_words(self, file_name):
+		self.words = open(file_name).readlines()
+		self.words = map(lambda x: x.strip(), self.words)
+	
+	def check_word(self, word):
+		return word.strip('.').lower() in self.words
+
+	def check_words(self, sentence):
+		words_to_check = sentence.split(' ')
+		failed_words = []
+		for word in words_to_check:
+			if not self.check_word(word):
+				print('Word is misspelt : ' + word)
+				failed_words.append(word)
+		return failed_words
+
+if __name__ == '__main__':
+	spell_check = SpellChecker()
+	spell_check.load_words("spell.words")		
+	print spell_check.check_word('zygotic')
+	print spell_check.check_word('mist')
+	print spell_check.check_words('zygotic mistasdas elementary')
+
